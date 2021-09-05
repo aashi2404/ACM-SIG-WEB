7ead3a1c0540066bdbd7e9e39b06369229201b3c
aashi2404 <90144112+aashi2404@users.noreply.github.com>
2021-09-05 23:56:21 +0530

    TASK 9

---
+++ Aashi Task9/index.mjs
@@ -0,0 +1,9 @@
+import fs, { writeFileSync } from "fs";
+// fs.mkdirSync("Nodejs");
+// fs.writeFileSync("Nodejs/aashi.txt", "This is a text file.");
+// fs.appendFileSync("Nodejs/aashi.txt", "It talks about Nodejs.");
+// const data = fs.readFileSync("Nodejs/aashi.txt", "utf-8");
+// console.log(data);
+// fs.renameSync("Nodejs/aashi.txt", "Nodejs/aashiGupta.txt");
+// fs.unlinkSync("Nodejs/aashiGupta.txt");
+fs.rmdirSync("Nodejs");
